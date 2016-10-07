e4d1e877f5d83c8d9ac26b9610fa08faf0cc043d
Dan Logan <dmlogs@gmail.com>
2016-10-07 14:18:45 -0700

    Refactoring tests to support changes made in services to extract request handling functionality

--- test/services/darkskyTest.js
+++ test/services/darkskyTest.js
@@ -1,144 +1,73 @@
 var chai = require('chai');
 var assert = chai.assert;
-var config = require('config');
 var fs = require('fs');
 
 describe("darksky", function() {
-    this.timeout(10000);
-    var darkskyWrapper = require('./../../services/darksky.js'),
-        MockReq = require('./../mocks/MockReq.js'),
-        MockRes = require('./../mocks/MockRes.js'),
+    var darkskyConstructor = require('./../../services/weather/darksky.js'),
+        MockFetcher = require('./../mocks/MockFetcher.js'),
         defaults = {
             "endpoint": "api.darksky.net",
             "pathRoot": "/forecast",
-            "secret": config.get("darksky.secret"),
+            "secret": "test",
             "exclude": "minutely,hourly,alerts,flags",
             "reportPath": "https://darksky.net"
         },
         lat = 45.5278,
         long = -122.8013;
 
-    function verifyResponseBody(data) {
-        assert.equal(lat, JSON.parse(data).latitude);
-    }
+    describe("#getPath()", function() {
+        var darksky = darkskyConstructor(defaults);
+        it('should return path with secret but without excludes', function() {
+            var result = darksky.getPath(defaults.pathRoot, lat, long, defaults.secret);
+            assert.equal(`${defaults.pathRoot}/${defaults.secret}/${lat},${long}`,result);
+        });
 
-    function verifyStandardizedResponse(result, latitude, longitude) {
-        latitude = latitude || 42.3601;
-        longitude = longitude || -71.0589;
+        it('should return path with secret and excludes', function() {
+            var result = darksky.getPath(defaults.pathRoot, lat, long, defaults.secret, defaults.exclude);
+            assert.equal(`${defaults.pathRoot}/${defaults.secret}/${lat},${long}?exclude=${defaults.exclude}`, result);
+        });
 
-        assert.equal(`https://darksky.net/${latitude},${longitude}`, result.report);
-        assert.isNotNull(result.lat);
-        assert.isNotNull(result.long);
-        assert.isNotNull(result.current);
-        assert.isNotNull(result.current.timestamp);
-        assert.isNotNull(result.current.temperature);
-        assert.isNotNull(result.current.summary);
-        assert.isNotNull(result.daily);
-        assert.equal(8, result.daily.length);
-    }
-
-    describe("#fetch()", function() {
-        it('should return the raw json from darksky', function(done) {
-            try {
-                var darksky = darkskyWrapper(defaults);
-                darksky.fetch(lat, long, function(data) {
-                    verifyResponseBody(data);
-                    done();
-                }, function(err) {
-                    assert.isTrue(false);
-                })
-            } catch (e) {
-                done(e);
-            }
+        it('should return path without secret or excludes', function() {
+            var result = darksky.getPath(defaults.reportPath, lat, long);
+            assert.equal(`${defaults.reportPath}/${lat},${long}`,result);
         });
     });
 
-    describe("#requestHandler()", function() {
-        it('should return the raw json in a response with status 200', function(done) {
-            try {
-                function validation(status, body) {
-                    assert.equal(status, 200);
-                    verifyResponseBody(body);
-                    done();
-                }
-
-                var darksky = darkskyWrapper(defaults),
-                    req = new MockReq(),
-                    res = new MockRes(validation);
-
-                req.setLatLong(lat, long);
-                darksky.requestHandler(req, res);
-            } catch (e) {
-                done(e);
-            }
-        });
-
-        it('should return the standardized json in a response with status 200', function(done) {
-            try {
-                function validation(status, body) {
-                    assert.equal(status, 200);
-                    verifyStandardizedResponse(body, lat, long);
-                    done();
-                }
-
-                var darksky = darkskyWrapper(defaults),
-                    req = new MockReq(),
-                    res = new MockRes(validation);
-
-                req.setLatLong(lat, long);
-                req.query.f = "std";
-                darksky.requestHandler(req, res);
-            } catch (e) {
-                done(e);
-            }
-        });
-
-        it('should return status 400 with body of \'missing latitude.\'', function(done) {
-            try {
-                function validation(status, body) {
-                    assert.equal(status, 400);
-                    assert.equal(body, "missing latitude.");
-                    done();
-                }
-
-                var darksky = darkskyWrapper(defaults),
-                    req = new MockReq(),
-                    res = new MockRes(validation);
-
-                req.setLong(long);
-                darksky.requestHandler(req, res);
-            } catch (e) {
-                done(e);
-            }
-        });
-
-        it('should return status 400 with body of \'missing longitude.\'', function(done) {
-            try {
-                function validation(status, body) {
-                    assert.equal(status, 400);
-                    assert.equal(body, "missing longitude.");
-                    done();
-                }
-
-                var darksky = darkskyWrapper(defaults),
-                    req = new MockReq(),
-                    res = new MockRes(validation);
-
-                req.setLat(lat);
-                darksky.requestHandler(req, res);
-            } catch (e) {
-                done(e);
-            }
+    describe("#fetch()", function() {
+        it('should build proper request and fetch', function(done) {
+            var fetch = new MockFetcher();
+            var darksky = darkskyConstructor(defaults, fetch);
+            darksky.fetch(lat, long, function(handler, rq) {
+                assert.equal("https", handler);
+                assert.equal(defaults.endpoint, rq.host);
+                assert.isNotNull(rq.path);
+                done();
+            }, function(err) {
+                assert.isTrue(false);
+            });
         });
     });
 
     describe("#standardizeResponse()", function() {
         var darkskySampleResponse = fs.readFileSync('./test/data/darkskySampleResponse.json');
 
-        it('should parse and standardize the response', function() {
-            var darksky = darkskyWrapper(defaults);
-            var result = darksky.standardizeResponse(darkskySampleResponse);
-            verifyStandardizedResponse(result);
+        it('should parse and standardize the response', function(done) {
+            var darksky = darkskyConstructor(defaults);
+            var result = darksky.standardizeResponse(darkskySampleResponse, function(result) {
+              var latitude = 42.3601,
+                  longitude = -71.0589;
+
+              assert.equal(`https://darksky.net/${latitude},${longitude}`, result.report);
+              assert.isNotNull(result.lat);
+              assert.isNotNull(result.long);
+              assert.isNotNull(result.current);
+              assert.isNotNull(result.current.timestamp);
+              assert.isNotNull(result.current.temperature);
+              assert.isNotNull(result.current.summary);
+              assert.isNotNull(result.daily);
+              assert.equal(8, result.daily.length);
+              done();
+            });
         });
     });
 });
--- test/services/weathergovTest.js
+++ test/services/weathergovTest.js
@@ -3,11 +3,8 @@ var assert = chai.assert;
 var fs = require('fs');
 
 describe("weathergov", function() {
-    this.timeout(10000);
-    var weathergovWrapper = require('./../../services/weathergov.js'),
-        MockReq = require('./../mocks/MockReq.js'),
-        MockRes = require('./../mocks/MockRes.js'),
-
+    var weathergovConstructor = require('./../../services/weather/weathergov.js'),
+        MockFetcher = require('./../mocks/MockFetcher.js'),
         defaults = {
             "endpoint": "forecast.weather.gov",
             "pathRoot": "/MapClick.php?",
@@ -18,158 +15,57 @@ describe("weathergov", function() {
         lat = 45.5278,
         long = -122.8013;
 
-    function verifyResponseBody(data) {
-        assert.isNotNull(JSON.parse(data).operationalMode);
-    }
 
-    function verifyStandardizedResponse(result, latitude, longitude) {
-        latitude = latitude || lat;
-        longitude = longitude || long;
-
-        assert.equal(`http://forecast.weather.gov/MapClick.php?lat=${latitude}&lon=${longitude}&unit=${defaults.unit}&lg=${defaults.lg}`,result.report);
-        assert.isNotNull(result.lat);
-        assert.isNotNull(result.long);
-        assert.isNotNull(result.current);
-        assert.isNotNull(result.current.timestamp);
-        assert.isNotNull(result.current.temperature);
-        assert.isNotNull(result.current.summary);
-        assert.isNotNull(result.current.daily);
-    }
 
     describe("#fetch()", function() {
-        it('should return the raw json from weather.gov', function(done) {
-            try {
-                var weathergov = weathergovWrapper(defaults);
-                weathergov.fetch(
-                    lat,
-                    long,
-                    defaults.units,
-                    defaults.lg,
-                    defaults.fcstType,
-                    function(data) {
-                        verifyResponseBody(data);
-                        done();
-                    },
-                    function(err) {
-                        assert.isTrue(false);
-                    }
-                );
-            } catch (e) {
-                done(e);
-            }
+        it('should build proper request and fetch', function(done) {
+          var fetch = new MockFetcher();
+          var weathergov = weathergovConstructor(defaults, fetch);
+          weathergov.fetch(lat, long, function(handler, rq) {
+              assert.equal("http", handler);
+              assert.equal(defaults.endpoint, rq.host);
+              assert.isNotNull(rq.path);
+              assert.equal("curl/7.43.0",rq.headers["User-Agent"]);
+              done();
+          }, function(err) {
+              assert.isTrue(false);
+          });
         });
     });
 
     describe("#getPath()", function() {
         it('should return the correct path with fcstType', function() {
-            var weathergov = weathergovWrapper(defaults);
-            var result = weathergov.getPath(1, 2, 3, 4, 5);
-            assert.equal("/MapClick.php?lat=1&lon=2&unit=3&lg=4&FcstType=5", result);
+            var weathergov = weathergovConstructor(defaults);
+            var result = weathergov.getPath(1, 2, true);
+            assert.equal(`/MapClick.php?lat=1&lon=2&unit=${defaults.unit}&lg=${defaults.lg}&FcstType=${defaults.fcstType}`, result);
         });
 
         it('should return the correct path', function() {
-            var weathergov = weathergovWrapper(defaults);
-            var result = weathergov.getPath(1, 2, 3, 4);
-            assert.equal("/MapClick.php?lat=1&lon=2&unit=3&lg=4", result);
-        });
-    });
-
-    describe("#requestHandler()", function() {
-        it('should return the raw json in a response with status 200', function(done) {
-            try {
-                function validation(status, body) {
-                    assert.equal(status, 200);
-                    verifyResponseBody(body);
-                    done();
-                }
-
-                var weathergov = weathergovWrapper(defaults),
-                    req = new MockReq(),
-                    res = new MockRes(validation);
-
-                req.setLatLong(lat, long);
-
-                weathergov.requestHandler(req, res);
-            } catch (e) {
-                done(e);
-            }
-        });
-
-        it('should return the standardized json in a response with status 200', function(done) {
-            try {
-                function validation(status, body) {
-                    assert.equal(status, 200);
-                    verifyStandardizedResponse(body, lat, long);
-                    done();
-                }
-
-                var weathergov = weathergovWrapper(defaults),
-                    req = new MockReq(),
-                    res = new MockRes(validation);
-
-                req.setLatLong(lat, long);
-                req.query.f = "std";
-                weathergov.requestHandler(req, res);
-            } catch (e) {
-                done(e);
-            }
-        });
-
-        it('should return status 400 with body of \'missing latitude.\'', function(done) {
-            try {
-                function validation(status, body) {
-                    assert.equal(status, 400);
-                    assert.equal(body, "missing latitude.");
-                    done();
-                }
-
-                var weathergov = weathergovWrapper(defaults),
-                    req = new MockReq(),
-                    res = new MockRes(validation);
-
-                req.setLong(long);
-
-                weathergov.requestHandler(req, res);
-            } catch (e) {
-                done(e);
-            }
-        });
-
-        it('should return status 400 with body of \'missing longitude.\'', function(done) {
-            try {
-                function validation(status, body) {
-                    assert.equal(status, 400);
-                    assert.equal(body, "missing longitude.");
-                    done();
-                }
-
-                var weathergov = weathergovWrapper(defaults),
-                    req = new MockReq(),
-                    res = new MockRes(validation);
-
-                req.setLat(lat);
-
-                weathergov.requestHandler(req, res);
-            } catch (e) {
-                done(e);
-            }
+            var weathergov = weathergovConstructor(defaults);
+            var result = weathergov.getPath(1, 2, false);
+            assert.equal(`/MapClick.php?lat=1&lon=2&unit=${defaults.unit}&lg=${defaults.lg}`, result);
         });
     });
 
     describe("#standardizeResponse()", function() {
         var weathergovSampleResponse = fs.readFileSync('./test/data/weathergovSampleResponse.json');
 
-        function verifyStandardizedResponseLastNoLow(data) {
-            assert.equal(7, data.daily.length);
-            var entry = data.daily[6];
-            assert.isNull(enty.temepratureMin);
-            assert.isNotNull(enty.temepratureMax);
-        }
-
-        it('should parse and standardize the response, last no low', function() {
-            var weathergov = weathergovWrapper(defaults);
-            var result = weathergov.standardizeResponse(weathergovSampleResponse,lat,long,defaults.unit,defaults.lg);
-            verifyStandardizedResponse(result);
+        it('should parse and standardize the response, last no low', function(done) {
+            var weathergov = weathergovConstructor(defaults);
+            var result = weathergov.standardizeResponse(weathergovSampleResponse,function(result) {
+              assert.equal(`http://forecast.weather.gov/MapClick.php?lat=${lat}&lon=${long}&unit=${defaults.unit}&lg=${defaults.lg}`,result.report);
+              assert.isNotNull(result.lat);
+              assert.isNotNull(result.long);
+              assert.isNotNull(result.current);
+              assert.isNotNull(result.current.timestamp);
+              assert.isNotNull(result.current.temperature);
+              assert.isNotNull(result.current.summary);
+              assert.equal(7, result.daily.length);
+              var entry = result.daily[6];
+              assert.isUndefined(entry.temperatureMin);
+              assert.isNotNull(entry.temperatureMax);
+              done();
+            },lat,long);
         });
     });
 });
