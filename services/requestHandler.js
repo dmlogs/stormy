72f1ca7bd6c9f605b67b0be9c87bdaf2510890e8
Dan Logan <dmlogs@gmail.com>
2016-10-07 17:29:15 -0700

    adding getAll unified api

--- services/requestHandler.js
+++ services/requestHandler.js
@@ -1,13 +1,15 @@
+var async = require('async');
+
 module.exports = (sources) => {
     return {
         get: function(req, res, source) {
             if (!source) {
-              res.status(400).send("no source provided.");
-              return;
+                res.status(400).send("no source provided.");
+                return;
             }
             if (!sources.hasOwnProperty(source)) {
-              res.status(400).send(`source "${source}" is unknown.`);
-              return;
+                res.status(400).send(`source "${source}" is unknown.`);
+                return;
             }
 
             if (!req.query.lat) {
@@ -19,28 +21,63 @@ module.exports = (sources) => {
                 return;
             }
 
+            function success(body) {
+                res.status(200).send(body);
+            }
+
+            function error(err) {
+                res.status(404).send(err);
+            }
+
             var src = sources[source],
-              lat = req.query.lat,
-              long = req.query.long;
-
-            function success (body) {
-              res.status(200).send(body);
-            }
-
-            function error (err) {
-              res.status(404).send(err);
-            }
+                lat = req.query.lat,
+                long = req.query.long;
 
             if (req.query.f == "std") {
                 try {
-                  src.getStandardized(lat,long, success, error);
+                    src.getStandardized(lat, long, success, error);
                 } catch (e) {
-                  res.status(500).send(`An error occurred processing the returned data: \n\n${e}`)
+                    res.status(500).send(`An error occurred processing the returned data: \n\n${e}`)
                 }
+            } else {
+                src.getRaw(lat, long, success, error);
             }
-            else {
-              src.getRaw(lat,long, success, error);
+        },
+        getAll: function(req, res) {
+            if (!req.query.lat) {
+                res.status(400).send("missing latitude.");
+                return;
+            };
+            if (!req.query.long) {
+                res.status(400).send("missing longitude.");
+                return;
             }
+
+            function success(body) {
+                res.status(200).send(body);
+            }
+
+            function error(err) {
+                res.status(404).send(err);
+            }
+
+            var lat = req.query.lat,
+                long = req.query.long;
+            var results = {},
+                errors = {};
+
+            async.each(Object.keys(sources), function(source, done) {
+                sources[source].getStandardized(lat, long, function(result) {
+                    results[source] = result;
+                    done();
+                }, function(err) {
+                    errors[source] = err;
+                    done();
+                });
+            }, function() {
+                if (Object.keys(errors).length > 0) results.error = errors;
+                success(results);
+            });
         }
     }
 }
--- test/services/requestHandlerTest.js
+++ test/services/requestHandlerTest.js
@@ -2,129 +2,207 @@ var chai = require('chai');
 var assert = chai.assert;
 
 describe("requestHandler", function() {
-  var requestHandlerConstructor = require('./../../services/requestHandler.js'),
-   MockReq = require('./../mocks/MockReq.js'),
-   MockRes = require('./../mocks/MockRes.js'),
-   MockService = require('./../mocks/MockService.js');
+    var requestHandlerConstructor = require('./../../services/requestHandler.js'),
+        MockReq = require('./../mocks/MockReq.js'),
+        MockRes = require('./../mocks/MockRes.js'),
+        MockService = require('./../mocks/MockService.js');
 
-  describe("#get()", function() {
-      it('source not provided',function(done) {
-        var requestHandler = requestHandlerConstructor();
-        var req = new MockReq();
-        var res = new MockRes(function(statusCode,data) {
-          assert.equal(400,statusCode);
-          assert.equal("no source provided.", data);
-          done();
+    describe("#get()", function() {
+        it('source not provided', function(done) {
+            var requestHandler = requestHandlerConstructor();
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(400, statusCode);
+                assert.equal("no source provided.", data);
+                done();
+            });
+            requestHandler.get(req, res);
         });
-        requestHandler.get(req,res);
-      });
 
-      it('source does not exist',function(done) {
-        var requestHandler = requestHandlerConstructor({});
-        var req = new MockReq();
-        var res = new MockRes(function(statusCode,data) {
-          assert.equal(400,statusCode);
-          assert.equal('source "notvalid" is unknown.', data);
-          done();
+        it('source does not exist', function(done) {
+            var requestHandler = requestHandlerConstructor({});
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(400, statusCode);
+                assert.equal('source "notvalid" is unknown.', data);
+                done();
+            });
+            requestHandler.get(req, res, "notvalid");
         });
-        requestHandler.get(req,res,"notvalid");
-      });
 
-      it('query parameter lat is missing',function(done) {
-        var requestHandler = requestHandlerConstructor({"exists":true});
-        var req = new MockReq();
-        var res = new MockRes(function(statusCode,data) {
-          assert.equal(400,statusCode);
-          assert.equal("missing latitude.", data);
-          done();
+        it('query parameter lat is missing', function(done) {
+            var requestHandler = requestHandlerConstructor({
+                "exists": true
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(400, statusCode);
+                assert.equal("missing latitude.", data);
+                done();
+            });
+            requestHandler.get(req, res, "exists");
         });
-        requestHandler.get(req,res,"exists");
-      });
 
-      it('query parameter long is missing',function(done) {
-        var requestHandler = requestHandlerConstructor({"exists":true});
-        var req = new MockReq();
-        var res = new MockRes(function(statusCode,data) {
-          assert.equal(400,statusCode);
-          assert.equal("missing longitude.", data);
-          done();
+        it('query parameter long is missing', function(done) {
+            var requestHandler = requestHandlerConstructor({
+                "exists": true
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(400, statusCode);
+                assert.equal("missing longitude.", data);
+                done();
+            });
+            req.setLat(100);
+            requestHandler.get(req, res, "exists");
         });
-        req.setLat(100);
-        requestHandler.get(req,res,"exists");
-      });
 
-      it('no query parameter f and success',function(done) {
-        var service = new MockService(function(type,lat,long,success,error) {
-          assert.equal('raw',type);
-          assert.equal(100,lat);
-          assert.equal(200,long);
-          success("success");
+        it('no query parameter f and success', function(done) {
+            var service = new MockService(function(type, lat, long, success, error) {
+                assert.equal('raw', type);
+                assert.equal(100, lat);
+                assert.equal(200, long);
+                success("success");
+            });
+            var requestHandler = requestHandlerConstructor({
+                "service": service
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(200, statusCode);
+                assert.equal("success", data);
+                done();
+            });
+            req.setLatLong(100, 200);
+            requestHandler.get(req, res, "service");
         });
-        var requestHandler = requestHandlerConstructor({"service":service});
-        var req = new MockReq();
-        var res = new MockRes(function(statusCode,data) {
-          assert.equal(200,statusCode);
-          assert.equal("success", data);
-          done();
-        });
-        req.setLatLong(100,200);
-        requestHandler.get(req,res,"service");
-      });
 
-      it('no query parameter f and error',function(done) {
-        var service = new MockService(function(type,lat,long,success,error) {
-          assert.equal('raw',type);
-          assert.equal(100,lat);
-          assert.equal(200,long);
-          error("error");
+        it('no query parameter f and error', function(done) {
+            var service = new MockService(function(type, lat, long, success, error) {
+                assert.equal('raw', type);
+                assert.equal(100, lat);
+                assert.equal(200, long);
+                error("error");
+            });
+            var requestHandler = requestHandlerConstructor({
+                "service": service
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(404, statusCode);
+                assert.equal("error", data);
+                done();
+            });
+            req.setLatLong(100, 200);
+            requestHandler.get(req, res, "service");
         });
-        var requestHandler = requestHandlerConstructor({"service":service});
-        var req = new MockReq();
-        var res = new MockRes(function(statusCode,data) {
-          assert.equal(404,statusCode);
-          assert.equal("error", data);
-          done();
-        });
-        req.setLatLong(100,200);
-        requestHandler.get(req,res,"service");
-      });
 
-      it('query parameter f is std',function(done) {
-        var service = new MockService(function(type,lat,long,success,error) {
-          assert.equal('std',type);
-          assert.equal(100,lat);
-          assert.equal(200,long);
-          success("success");
+        it('query parameter f is std', function(done) {
+            var service = new MockService(function(type, lat, long, success, error) {
+                assert.equal('std', type);
+                assert.equal(100, lat);
+                assert.equal(200, long);
+                success("success");
+            });
+            var requestHandler = requestHandlerConstructor({
+                "service": service
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(200, statusCode);
+                assert.equal("success", data);
+                done();
+            });
+            req.setLatLong(100, 200);
+            req.setStd();
+            requestHandler.get(req, res, "service");
         });
-        var requestHandler = requestHandlerConstructor({"service":service});
-        var req = new MockReq();
-        var res = new MockRes(function(statusCode,data) {
-          assert.equal(200,statusCode);
-          assert.equal("success", data);
-          done();
-        });
-        req.setLatLong(100,200);
-        req.setStd();
-        requestHandler.get(req,res,"service");
-      });
 
-      it('query parameter f is std, but error occurs in getStandardized', function(done) {
-        var service = new MockService(function(type,lat,long,success,error) {
-          assert.equal('std',type);
-          assert.equal(100,lat);
-          assert.equal(200,long);
-          throw "error";
+        it('query parameter f is std, but error occurs in getStandardized', function(done) {
+            var service = new MockService(function(type, lat, long, success, error) {
+                assert.equal('std', type);
+                assert.equal(100, lat);
+                assert.equal(200, long);
+                throw "error";
+            });
+            var requestHandler = requestHandlerConstructor({
+                "service": service
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(500, statusCode);
+                assert.equal("An error occurred processing the returned data: \n\nerror", data);
+                done();
+            });
+            req.setLatLong(100, 200);
+            req.setStd();
+            requestHandler.get(req, res, "service");
         });
-        var requestHandler = requestHandlerConstructor({"service":service});
-        var req = new MockReq();
-        var res = new MockRes(function(statusCode,data) {
-          assert.equal(500,statusCode);
-          assert.equal("An error occurred processing the returned data: \n\nerror", data);
-          done();
+    });
+
+    describe("#getAll()", function() {
+        it('query parameter lat is missing', function(done) {
+            var requestHandler = requestHandlerConstructor({
+                "exists": true
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(400, statusCode);
+                assert.equal("missing latitude.", data);
+                done();
+            });
+            requestHandler.getAll(req, res);
         });
-        req.setLatLong(100,200);
-        req.setStd();
-        requestHandler.get(req,res,"service");
-      });
-  });
+        it('query parameter long is missing', function(done) {
+            var requestHandler = requestHandlerConstructor({
+                "exists": true
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(400, statusCode);
+                assert.equal("missing longitude.", data);
+                done();
+            });
+            req.setLat(100);
+            requestHandler.getAll(req, res);
+        });
+        it('should get standardized successfully for all sources', function(done) {
+            var mockService = new MockService(function(type, lat, long, success, error) {
+                assert.equal('std', type);
+                assert.equal(100, lat);
+                assert.equal(200, long);
+                success("test");
+            });
+            var requestHandler = requestHandlerConstructor({
+                "service": mockService
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(200, statusCode);
+                assert.equal("test", data.service);
+                done();
+            });
+            req.setLatLong(100, 200);
+            requestHandler.getAll(req, res);
+        });
+        it('should get standardized errors for all sources', function(done) {
+            var mockService = new MockService(function(type, lat, long, success, error) {
+                assert.equal('std', type);
+                assert.equal(100, lat);
+                assert.equal(200, long);
+                error("test");
+            });
+            var requestHandler = requestHandlerConstructor({
+                "service": mockService
+            });
+            var req = new MockReq();
+            var res = new MockRes(function(statusCode, data) {
+                assert.equal(200, statusCode);
+                assert.equal("test", data.error.service);
+                done();
+            });
+            req.setLatLong(100, 200);
+            requestHandler.getAll(req, res);
+        });
+    });
 });
